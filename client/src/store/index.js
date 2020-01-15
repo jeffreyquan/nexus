a38256debe4c3937123c42482756f20538ebae7a
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-01-15 12:42:13 +1100

    set up store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,15 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { FeathersVuex } from '../feathers-client';
+import auth from './store.auth';
+import users from './services/users';
 
 Vue.use(Vuex);
+Vue.use(FeathersVuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
+  plugins: [users, auth],
 });

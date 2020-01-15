18771142c569cd9b8c3866095f9df3a1e5a3e921
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-01-15 12:39:10 +1100

    initialised board view

---
+++ client/src/views/Boards.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <v-progress-circular
+          :size="50"
+          color="primary"
+          indeterminate
+         ></v-progress-circular>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>

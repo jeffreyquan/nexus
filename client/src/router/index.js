9ae775ed34c3befee2028be81d0d0b609bedeca6
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-01-15 12:42:47 +1100

    set up new routers into redirects depending on login status

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import SignUp from '../views/SignUp.vue';
+import Login from '../views/Login.vue';
+import Boards from '../views/Boards.vue';
+
+import store from '../store/index';
 
 Vue.use(VueRouter);
 
@@ -9,6 +14,13 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home,
+    beforeEnter(to, from, next) {
+      store.dispatch('auth/authenticate').then(() => {
+        next('/boards');
+      }).catch(() => {
+        next('/login');
+      });
+    },
   },
   {
     path: '/about',
@@ -17,6 +29,28 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: SignUp,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+  },
+  {
+    path: '/boards',
+    name: 'boards',
+    component: Boards,
+    beforeEnter(to, from, next) {
+      store.dispatch('auth/authenticate').then(() => {
+        next();
+      }).catch(() => {
+        next('/login');
+      });
+    },
   },
 ];
 

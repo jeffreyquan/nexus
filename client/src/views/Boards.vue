4653e7fc92e960536f62c3a567083e70bad627a8
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-01-15 23:46:54 +1100

    able to create board

--- client/src/views/Boards.vue
+++ client/src/views/Boards.vue
@@ -11,6 +11,33 @@
          <pre v-if="!loading">
            {{ boards }}
          </pre>
+          <v-flex sm4>
+            <v-card
+            class="mx-auto"
+            max-width="400"
+          >
+            <v-card-text class="text--primary">
+              <div>Create Board</div>
+              <div>
+                <v-form
+                  v-if="!creating"
+                  @submit.prevent="createBoard"
+                  @keydown.prevent.enter
+                  ref="form"
+                  v-model="valid"
+                >
+                  <v-text-field
+                    v-model="board.name"
+                    :rules="nameRules"
+                    label="Name"
+                    required
+                  ></v-text-field>
+                  <v-btn type="submit" :disabled="!valid">Create</v-btn>
+                </v-form>
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-flex>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
@@ -21,6 +48,16 @@ import { mapActions, mapGetters, mapState } from 'vuex';
 
 export default {
   name: 'boards',
+  data: () => ({
+    creating: false,
+    valid: false,
+    board: {
+      name: '',
+    },
+    nameRules: [
+      v => !!v || 'Name is required',
+    ],
+  }),
   mounted() {
     this.findBoards({ query: {} })
       .then((response) => {
@@ -31,12 +68,20 @@ export default {
   },
   methods: {
     ...mapActions('boards', { findBoards: 'find' }),
+    createBoard() {
+      if (this.valid) {
+        const { Board } = this.$FeathersVuex.api;
+        console.log(Board);
+        const board = new Board(this.board);
+        board.save();
+      }
+    },
   },
   computed: {
     ...mapState('boards', { loading: 'isFindPending' }),
     ...mapGetters('boards', { findBoardsInStore: 'find' }),
     boards() {
-      return this.findBoardsInStore({ query: {} });
+      return this.findBoardsInStore({ query: {} }).data;
     },
   },
 };

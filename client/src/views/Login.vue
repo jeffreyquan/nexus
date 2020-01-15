07a8b49df3f5e67a37590ecaedd6ca9f14f6edc9
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-01-15 12:37:55 +1100

    created login view

---
+++ client/src/views/Login.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <v-form
+          v-if="!loading"
+          @submit.prevent="login"
+          @keydown.prevent.enter
+          ref="form"
+          v-model="valid"
+        >
+          <v-text-field
+            v-model="user.email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="user.password"
+            :rules="passwordRules"
+            label="Password"
+            type="password"
+            required
+          ></v-text-field>
+          <v-btn type="submit" :disabled="!valid">Login</v-btn>
+        </v-form>
+        <v-progress-circular
+          v-if="loading"
+          :size="50"
+          color="primary"
+          indeterminate
+         ></v-progress-circular>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex';
+
+export default {
+  name: 'signUp',
+  data: () => ({
+    valid: false,
+    user: {
+      email: '',
+      password: '',
+    },
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+    ],
+    passwordRules: [
+      v => !!v || 'Password is required',
+    ],
+  }),
+  computed: {
+    ...mapState('users', { loading: 'isAuthenticatePending' }),
+  },
+  methods: {
+
+    ...mapActions('auth', ['authenticate']),
+
+    login() {
+      if (this.valid) {
+        this.authenticate({
+          strategy: 'local',
+          ...this.user,
+        }).then(() => {
+          this.$router.push('/boards');
+        }).catch(() => {
+          console.log('error....');
+        });
+      }
+    },
+  },
+};
+</script>

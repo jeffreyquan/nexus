b087f43bca0cab1009fafd602c208f1807f55a10
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-01-15 23:47:08 +1100

    delete unnecessary empty line

--- server/src/models/users.model.js
+++ server/src/models/users.model.js
@@ -6,7 +6,6 @@ module.exports = function (app) {
   const modelName = 'users';
   const mongooseClient = app.get('mongooseClient');
   const schema = new mongooseClient.Schema({
-  
     email: { 
       type: String,
       unique: true,

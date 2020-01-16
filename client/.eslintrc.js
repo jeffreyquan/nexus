52851382f6551a860b7859d09236095d588ecfff
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-01-17 01:35:13 +1100

    updated eslint rules to allow _

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
   extends: ["plugin:vue/essential", "@vue/airbnb"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-underscore-dangle": ["error", { "allow": ["_id"] }],
   },
   parserOptions: {
     parser: "babel-eslint"

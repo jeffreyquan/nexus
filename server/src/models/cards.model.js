f32abf954cb9cc6cdc53486b19bdb0fdf2e4c9b6
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-01-17 01:35:52 +1100

    generated card service

---
+++ server/src/models/cards.model.js
@@ -0,0 +1,50 @@
+// cards-model.js - A mongoose model
+// 
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const modelName = 'cards';
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+  const schema = new Schema({
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+    },
+    order: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    archived: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    boardId: {
+      type: Schema.Types.ObjectId,
+      ref: 'boards'
+    },
+    listId: {
+      type: Schema.Types.ObjectId,
+      ref: 'lists'
+    },
+    members: [{
+      type: Schema.Types.ObjectId,
+      ref: 'users'
+    }],
+  }, {
+    timestamps: true
+  });
+
+  // This is necessary to avoid model compilation errors in watch mode
+  // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
+  if (mongooseClient.modelNames().includes(modelName)) {
+    mongooseClient.deleteModel(modelName);
+  }
+  return mongooseClient.model(modelName, schema);
+  
+};

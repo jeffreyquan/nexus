985f7fe0161b2db9fbeb7e0a3fc386ed6632e312
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-01-17 01:36:27 +1100

    created create card component

---
+++ client/src/components/CreateCard.vue
@@ -0,0 +1,70 @@
+<template>
+<v-flex sm12 pa-2>
+  <v-card
+    class="mx-auto"
+    max-width="400"
+  >
+    <v-card-text class="text--primary">
+      <div>Create Card</div>
+        <div>
+          <v-form
+            v-if="!creatingCard"
+            v-model="validCard"
+            ref="form"
+            @submit.prevent="createCard"
+            @keydown.prevent.enter
+          >
+            <v-text-field
+              v-model="card.title"
+              :rules="titleRules"
+              label="Title"
+              required
+            ></v-text-field>
+            <v-btn type="submit" :disabled="!validCard">Create Card</v-btn>
+          </v-form>
+          <v-progress-circular
+            v-if="creatingCard"
+            :size="50"
+            color="primary"
+            indeterminate
+          ></v-progress-circular>
+        </div>
+      </v-card-text>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  name: 'create-card',
+  props: ['listId', 'boardId'],
+  data: () => ({
+    creatingCard: false,
+    validCard: false,
+    card: {
+      title: '',
+      members: [],
+    },
+    titleRules: [
+      v => !!v || 'Name is required',
+    ],
+  }),
+  methods: {
+    async createCard() {
+      if (this.validCard) {
+        const { Card } = this.$FeathersVuex.api;
+        this.card.boardId = this.boardId;
+        this.card.listId = this.listId;
+        const card = new Card(this.card);
+        this.creatingCard = true;
+        await card.save();
+        this.creatingCard = false;
+        this.card = {
+          title: '',
+          members: [],
+        };
+      }
+    },
+  },
+};
+</script>

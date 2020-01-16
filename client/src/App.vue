541ea4b03606faae09c364766859fb563624266a
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-01-17 01:38:47 +1100

    showing name in top bar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,24 +1,21 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-toolbar-title>Nexus</v-toolbar-title>
       </div>
-
       <v-spacer></v-spacer>
       <v-toolbar-items v-if="!user">
         <v-btn :to="{ name: 'login' }">Login</v-btn>
         <v-btn :to="{ name: 'signup' }">Sign Up</v-btn>
       </v-toolbar-items>
       <v-toolbar-items v-if="user">
+        <v-layout justify-content align-center pa-2>
+          <span class="white--text headline">{{ user.user.name }}</span>
+        </v-layout>
         <v-btn @click="logout">Logout</v-btn>
       </v-toolbar-items>
     </v-app-bar>
-
     <v-content>
       <router-view/>
     </v-content>

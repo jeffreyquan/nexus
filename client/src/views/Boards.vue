3e5262888b8c2f0bc870a9af0640d8a2ca21c766
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-01-19 03:32:16 +1100

    refactored out board tiles and board form components

--- client/src/views/Boards.vue
+++ client/src/views/Boards.vue
@@ -9,49 +9,17 @@
           color="primary"
           indeterminate
          ></v-progress-circular>
-          <v-flex sm3 v-for="board in boards" :key="board._id" pa-2>
-            <v-card v-if="!loading">
-              <v-card-title primary-title>
-                <div class="headline">{{ board.name }}</div>
-              </v-card-title>
-              <v-card-actions>
-                <v-btn color="primary" :to="{ name: 'board', params: { id: board._id } }">Go</v-btn>
-              </v-card-actions>
-            </v-card>
+          <v-flex row v-if="!loading">
+            <v-flex sm3 v-for="board in boards" :key="board._id" pa-2>
+              <board-tile :board="board"></board-tile>
+            </v-flex>
           </v-flex>
           <v-flex sm4 pa-2>
-            <v-card
-            class="mx-auto"
-            max-width="400"
-          >
-            <v-card-text class="text--primary">
-              <div>Create Board</div>
-              <div>
-                <v-form
-                  v-if="!creating"
-                  v-model="valid"
-                  ref="form"
-                  @submit.prevent="createBoard"
-                  @keydown.prevent.enter
-                >
-                  <v-text-field
-                    v-model="board.name"
-                    :rules="nameRules"
-                    label="Name"
-                    required
-                  ></v-text-field>
-                  <v-btn type="submit" :disabled="!valid">Create</v-btn>
-                </v-form>
-                <v-progress-circular
-                  v-if="creating"
-                  :size="50"
-                  color="primary"
-                  indeterminate
-                ></v-progress-circular>
-              </div>
-            </v-card-text>
-          </v-card>
-        </v-flex>
+            <board-form
+              :creating="creating"
+              :createBoard="createBoard"
+            ></board-form>
+          </v-flex>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
@@ -59,38 +27,24 @@
 
 <script>
 import { mapActions, mapGetters, mapState } from 'vuex';
+import BoardTile from '../components/BoardTile.vue';
+import BoardForm from '../components/BoardForm.vue';
 
 export default {
   name: 'boards',
-  data: () => ({
-    valid: false,
-    board: {
-      name: '',
-    },
-    nameRules: [
-      v => !!v || 'Name is required',
-    ],
-  }),
+  components: {
+    BoardTile,
+    BoardForm,
+  },
   mounted() {
-    this.findBoards({ query: {} })
-      .then((response) => {
-        console.log(response);
-        const boards = response.data || response;
-        console.log(boards);
-      });
+    this.findBoards({ query: {} });
   },
   methods: {
     ...mapActions('boards', { findBoards: 'find' }),
-    createBoard() {
-      if (this.valid) {
-        const { Board } = this.$FeathersVuex.api;
-        console.log(Board);
-        const board = new Board(this.board);
-        board.save();
-        this.board = {
-          name: '',
-        };
-      }
+    async createBoard(board) {
+      const { Board } = this.$FeathersVuex.api;
+      const newBoard = new Board(board);
+      await newBoard.save();
     },
   },
   computed: {

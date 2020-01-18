b05d200ba4c8e3fce02ca108be6cf59245c6fbc4
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-01-18 22:18:15 +1100

    added board authorisation to card service

--- server/src/services/cards/cards.hooks.js
+++ server/src/services/cards/cards.hooks.js
@@ -1,8 +1,9 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
+const { isBoardOwner } = require('../authorisation');
 
 module.exports = {
   before: {
-    all: [ authenticate('jwt') ],
+    all: [ authenticate('jwt'), isBoardOwner ],
     find: [],
     get: [],
     create: [],

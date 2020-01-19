d570fcc388adafb9841542463df9c2146e58dab1
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-01-20 01:15:34 +1100

    trying to fix rule to allow hover effect on list

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-underscore-dangle": 0,
-    "vue/valid-v-on": 0,
+    "vue/valid-v-on": "off",
   },
   parserOptions: {
     parser: "babel-eslint"

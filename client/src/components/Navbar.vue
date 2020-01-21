101c69d18f1efc41ff30f21985f5b1ccc5e7f326
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-01-21 22:40:29 +1100

    styling to navbar

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,20 +1,18 @@
 <template>
   <v-app-bar app color="primary" dark>
-    <div class="d-flex align-center">
-      <v-toolbar-title>
-        <v-btn :to="{ name: 'home' }">Nexus</v-btn>
-      </v-toolbar-title>
-    </div>
+    <v-toolbar-title>
+      <v-btn :to="{ name: 'home' }">Nexus</v-btn>
+    </v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items v-if="!user">
-      <v-btn :to="{ name: 'login' }">Login</v-btn>
-      <v-btn :to="{ name: 'signup' }">Sign Up</v-btn>
+      <v-btn :to="{ name: 'login' }" text>Login</v-btn>
+      <v-btn :to="{ name: 'signup' }" text>Sign Up</v-btn>
     </v-toolbar-items>
     <v-toolbar-items v-if="user">
-      <v-layout justify-content align-center pa-2>
-        <span class="white--text headline">{{ user.user.name }}</span>
-      </v-layout>
-      <v-btn @click="logout">Logout</v-btn>
+      <v-row align="center">
+        <span class="title pa-1 mr-2 name-tag">{{ user.user.name }}</span>
+      </v-row>
+      <v-btn @click="logout" class="ml-1" text>Logout</v-btn>
     </v-toolbar-items>
   </v-app-bar>
 </template>
@@ -24,3 +22,9 @@ export default {
   props: ['user', 'logout'],
 };
 </script>
+
+<style scoped>
+.name-tag {
+  color: black;
+}
+</style>

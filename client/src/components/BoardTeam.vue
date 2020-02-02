17c30b22ba5736c56c4384295925d9fda5d1f05f
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-02-03 00:15:52 +1100

    drop down showing team

---
+++ client/src/components/BoardTeam.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="text-center">
+    <v-menu offset-y>
+      <template v-slot:activator="{ on }">
+        <v-btn
+          v-on="on"
+        >
+          Team
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="user in this.users"
+          :key="user._id"
+          @click=""
+        >
+          <v-list-item-title>{{ user.name }} ({{ user.email }})</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'board-team',
+  props: ['users'],
+};
+</script>
